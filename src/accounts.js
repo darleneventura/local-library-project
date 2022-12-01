5593f2f0a5bf0f7d935e05bfb736242391bdbeaf
Darlene Ventura <112041178+darleneventura@users.noreply.github.com>
2022-12-01 00:51:54 -0500

    Update accounts.js
    add reduce method and helper function

--- src/accounts.js
+++ src/accounts.js
@@ -11,42 +11,34 @@ function sortAccountsByLastName(accounts) {
 }
 
 function getTotalNumberOfBorrows(account, books) {
-   let totalBorrows = 0;
- for (let i = 0; i < books.length; i++) {
-  for (let j = 0; j < books[i].borrows.length; j++) {
-   if (account.id === books[i].borrows[j].id) {
-    totalBorrows += 1;
-   }
-  }
- }
- return totalBorrows;
+ let { id: accountId } = account;
+   return books.reduce((accumulator, book) => {
+     return (accumulator + book.borrows
+             .filter(borrow => borrow.id === accountId)
+             .reduce((accumulatorBorrows, borrow) => accumulatorBorrows + 1, 0)
+        );
+    }, 0);
 }
 
 function getBooksPossessedByAccount(account, books, authors) {
+  let  borrowedBooks = books.filter(book => book.borrows.some(borrow => (!borrow.returned && borrow.id === account.id)));
   let result = [];
- let borrowMatch = [];
- books.forEach((item) => {
-  const borrowed = item.borrows;
-  const book = {
-   id: item.id,
-   title: item.title,
-   genre: item.genre,
-   authorId: item.authorId,
-   author: {},
-   borrows: {}
-  };
-  const { id, title, genre, authorId, author, borrows } = book;
-
-  borrowed.forEach((borrow) => {
-   if (borrow.id === account.id && borrow.returned === false) {
-    result.push(book);
-    borrowMatch.push(borrow);
-    book.borrows = borrowMatch;
-    book.author = authors.filter((auth) => auth.id === book.authorId)[0];
-   }
+  let { findAuthorById } = require("./books");
+  borrowedBooks.forEach(book => {
+    let bookAuthor = findAuthorById(authors, book.authorId);
+    result.push({
+      id: book.id,
+      title: book.title,
+      genre: book.genre,
+      authorId: book.authorId,
+      author: bookAuthor,
+      borrows: book.borrows,
+    });
   });
- });
- return result;
+  return result;
+}
+function borrowsById (book, {id}) {
+  return book.borrows.filter(borrow => borrow.id === id);
 }
 
 module.exports = {
